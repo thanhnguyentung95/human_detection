4eeccaf6e9a60d91fb6d416394b6edb4bdb1a094
thannguyentung95 <thanhnguyentung95@gmail.com>
2021-04-01 22:38:25 +0900

    update model.py

--- model.py
+++ model.py
@@ -2,6 +2,47 @@ import torch
 from torch import nn
 
 
+class Yolov4Tiny(nn.Module):
+    def __init__(self):
+        ''' References ./cfg/yolov4-tiny.cfg '''
+        super(Yolov4Tiny, self).__init__()
+
+        # Backbone
+        # Conv2d(..., stride=2, padding=1, dilation=1, ..., batchnorm=True)
+        self.conv1 = Conv2d(3, 32, 3, stride=2, activation='leaky'),        # 25
+        self.conv2 = Conv2d(32, 64, 3, stride=2, activation='leaky'),       # 33
+        self.conv3 = Conv2d(64, 64, 3, stride=1, activation='leaky'),       # 41
+        self.route1 = Route(groups=2, group_id=1)                           # 49
+        self.conv4 = Conv2d(32, 32, 3, stride=1, activation='leaky')        # 54
+        self.conv5 = Conv2d(32, 32, 3, stride=1)                            # 62
+        self.route2 = Route()                                               # 70
+        self.conv6 = Conv2d(64, 64, 1, stride=1, activation='leaky')        # 73
+        self.route3 = Route()                                               # 81
+        self.maxpool1 = nn.MaxPool2d(2, stride=2)                           # 84
+        self.conv7 = Conv2d(128, 128, 3, stride=1, activation='leaky')      # 88
+        self.route4 = Route(groups=2, group_id=1)                           # 96
+        self.conv8 = Conv2d(64, 64, 3, stride=1, activation='leaky')        # 101
+        self.conv9 = Conv2d(64, 64, 3, stride=1, activation='leaky')        # 109
+        self.route5 = Route()                                               # 117
+        self.conv10 = Conv2d(128, 128, 1, stride=1, activation='leaky')     # 120
+        self.route6 = Route()                                               # 128
+        self.maxpool2 = nn.MaxPool2d(2, stride=2)                           # 131
+        self.conv11 = Conv2d(256, 256, 3, stride=1, activation='leaky')     # 135
+        self.route7 = Route(groups=2, group_id=1)                           # 143
+        self.conv12 = Conv2d(128, 128, 3, stride=1, activation='leaky')     # 148
+        self.conv13 = Conv2d(128, 128, 3, stride=1, activation='leaky')     # 156
+        self.route8 = Route()                                               # 164
+        self.conv14 = Conv2d(256, 256, 1, stride=1, activation='leaky')     # 167 
+        self.route9 = Route()                                               # 175
+        self.maxpool3 = nn.MaxPool2d(2, stride=2)                           # 178
+        self.conv15 = Conv2d(512, 512, 3, stride=1, activation='leaky')     # 182
+
+        # Neck
+        self.conv16 = Conv2d(512, 256, 1, stride=1, activation='leaky')     # 192
+        self.conv17 = Conv2d(256, 512, 3, stride=1, activation='leaky')     # 200
+        self.conv18 = Conv2d(512, 18, stride=1, activation=None)            # 208
+
+
 class Route(nn.Module):
     def __init__(self, groups=1, group_id=1):
         super(Route, self).__init__()
@@ -44,42 +85,6 @@ class Conv2d(nn.Module):
         return x
 
 
-class Yolov4Tiny(nn.Module):
-    def __init__(self):
-        ''' References ./cfg/yolov4-tiny.cfg '''
-        super(Yolov4Tiny, self).__init__()
-
-        # Backbone
-        # Conv2d(..., stride=2, padding=1, dilation=1, ..., batchnorm=True)
-        self.conv1 = Conv2d(3, 32, 3, stride=2, activation='leaky'),        # 25
-        self.conv2 = Conv2d(32, 64, 3, stride=2, activation='leaky'),       # 33
-        self.conv3 = Conv2d(64, 64, 3, stride=1, activation='leaky'),       # 41
-        self.route1 = Route(groups=2, group_id=1)                           # 49
-        self.conv4 = Conv2d(32, 32, 3, stride=1, activation='leaky')        # 54
-        self.conv5 = Conv2d(32, 32, 3, stride=1)                            # 62
-        self.route2 = Route()                                               # 70
-        self.conv6 = Conv2d(64, 64, 1, stride=1, activation='leaky')        # 73
-        self.route3 = Route()                                               # 81
-        self.maxpool1 = nn.MaxPool2d(2, stride=2)                           # 84
-        self.conv7 = Conv2d(128, 128, 3, stride=1, activation='leaky')      # 88
-        self.route4 = Route(groups=2, group_id=1)                           # 96
-        self.conv8 = Conv2d(64, 64, 3, stride=1, activation='leaky')        # 101
-        self.conv9 = Conv2d(64, 64, 3, stride=1, activation='leaky')        # 109
-        self.route5 = Route()                                               # 117
-        self.conv10 = Conv2d(128, 128, 1, stride=1, activation='leaky')     # 120
-        self.route6 = Route()                                               # 128
-        self.maxpool2 = nn.MaxPool2d(2, stride=2)                           # 131
-        self.conv11 = Conv2d(256, 256, 3, stride=1, activation='leaky')     # 135
-        self.route7 = Route(groups=2, group_id=1)                           # 143
-        self.conv12 = Conv2d(128, 128, 3, stride=1, activation='leaky')     # 148
-        self.conv13 = Conv2d(128, 128, 3, stride=1, activation='leaky')     # 156
-        self.route8 = Route()                                               # 164
-        self.conv14 = Conv2d(256, 256, 1, stride=1, activation='leaky')     # 167 
-        self.route9 = Route()                                               # 175
-        self.maxpool3 = nn.MaxPool2d(2, stride=2)                           # 178
-        self.conv15 = Conv2d(512, 512, 3, stride=1, activation='leaky')     # 182
-
-        # Neck
-        self.conv16 = Conv2d(512, 256, 1, stride=1, activation='leaky')     # 192
-        self.conv17 = Conv2d(256, 512, 3, stride=1, activation='leaky')     # 200
-        self.conv18 = Conv2d(512, 255, stride=1, activation=None)           # 208
+class YoloLayer(nn.Module):
+    def __init__(self, anchors, nc, img_size, yolo_index, layers, stride):
+        super(YoloLayer, self).__init__()
