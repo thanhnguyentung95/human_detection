e5ca71a22536199415668753fa837c96bc4db3dc
thanhnguyen <thanhnguyentung95@gmail.com>
2021-04-07 14:26:00 +0700

    add code to data.py

---
+++ utils/data.py
@@ -0,0 +1,15 @@
+from torch.utils.data import Dataset
+
+
+class Data(Dataset):
+    def __init__(self, path, img_size=640):
+        pass
+
+    
+    def __len__(self):
+        pass
+
+
+    def __getitem__(self, item):
+        pass
+
