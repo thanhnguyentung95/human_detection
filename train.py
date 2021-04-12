b50c01c5061f5ca69b6b93690005e286bf31e665
quandv <quandv@heligate.com.vn>
2021-04-13 00:15:59 +0700

    update data.py

---
+++ train.py
@@ -0,0 +1,5 @@
+from utils.data import Data
+
+train_path = '/home/heligate/Documents/ScaledYOLOv4/coco/train2017.txt'
+
+dataset =  Data(train_path)
--- utils/data.py
+++ utils/data.py
@@ -1,13 +1,24 @@
 from torch.utils.data import Dataset
-
+import os
 
 class Data(Dataset):
     def __init__(self, path, img_size=640):
-        pass
+        # Assert if file exist
+        assert os.path.isfile(path), '"{}" does not exist'.format(path)
 
+        # Get path dataset folder
+        dirname = os.path.dirname(path)
+
+        # Load image paths
+        with open(path) as f:
+            self.img_paths = f.readlines()
+            self.img_paths = [p.replace('./', dirname).strip() for p in self.img_paths]
+
+        self.len = len(img_paths)
+    
     
     def __len__(self):
-        pass
+        return self.len
 
 
     def __getitem__(self, item):
