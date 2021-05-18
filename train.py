5a1e712e744e74da4924f3f11206979b950d26ee
quandv <quandv@heligate.com.vn>
2021-05-18 09:39:24 +0700

    save and load model

--- train.py
+++ train.py
@@ -1,19 +1,25 @@
 import cv2
+import os
 import math
 import torch
 import numpy as np
+import yaml
 from utils.data import create_dataloader
 from models.model import Yolov4Tiny
 from torch.optim import lr_scheduler
 from utils.loss import compute_loss
 from torch import nn
 import matplotlib.pyplot as plt
+from utils.helper import create_exp_folder
+from tqdm import tqdm
 
-# HARD CODE area
 # path to list image file
 train_path = '/home/heligate/Documents/ScaledYOLOv4/coco/train2017.txt'
-lr = 0.00261
-momentum = 0.9
+
+# load hyper parameters
+with open('cfg/hyp.yaml', 'r') as f:
+    hyp = yaml.load(f, Loader=yaml.FullLoader)
+
 batch_size = 32
 img_size=416
 epochs = 20
@@ -23,26 +29,21 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # create data loader, dataset information
 dataloader = create_dataloader(train_path, batch_size=batch_size, img_size=img_size)
-nb = len(dataloader.dataset)   # number of batches 
+nb = len(dataloader)   # number of batches 
 
 # create model
 model = Yolov4Tiny().to(device)
-
-# hyperparameter
-hyp = {}
-hyp['nc'] = 80
-hyp['anchor_t'] = 4.0
 model.hyp = hyp
 
-# Gradient descent
 # create optimizer 
-optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=momentum, nesterov=True, weight_decay=0.0005)
+optimizer = torch.optim.SGD(model.parameters(), lr=hyp['lr0'], momentum=hyp['momentum'], nesterov=True, weight_decay=0.0005)
 
 # learning rate scheduling
 lf = lambda epoch: ((1 + math.cos(epoch * math.pi / epochs))/2) * 0.99 + 0.01
 scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lf)
 
 # warm-up
+lr = hyp['lr0'] # initial learning rate
 wue = 3 # warm-up epochs
 wub = wue * nb # warm-up batches
 wu_batchs = [0, wub] # warm-up range (in batch)
@@ -53,11 +54,25 @@ wu_lr = [0.01 * lr, max_lr] # warm-up range (in learning rate)
 n_batch_size = 64
 accumulate = n_batch_size / batch_size
 
+# resume
+start_epoch = 0
+model_name = 'yolov4_tiny'
+ckpt_path = 'runs/9_yolov4_tiny/checkpoint.pth'
+if ckpt_path:
+    checkpoint = torch.load(ckpt_path)
+    start_epoch = checkpoint['epoch'] + 1
+    model.load_state_dict(checkpoint['model'])
+    optimizer.load_state_dict(checkpoint['optimizer'])
+    scheduler.load_state_dict(checkpoint['scheduler'])
+
 # train
-for epoch in range(epochs):
+for epoch in range(start_epoch, epochs):
     model.train()
     # start epoch
-    for i, (imgs, labels) in enumerate(dataloader):
+    progress = tqdm(dataloader, total=nb)
+    title = '%10s' * 5 % ('Epoch', 'lobj', 'lcls', 'lbox', 'loss')
+    print(title)
+    for i, (imgs, labels) in enumerate(progress):
         # start batch
         batch = i + epoch * nb
         
@@ -70,14 +85,25 @@ for epoch in range(epochs):
         preds = model(imgs.float().to(device))
         
         loss, loss_item = compute_loss(preds, labels.to(device), model)                 
-        
+        lobj, lcls, lbox, total_loss = loss_item
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        print(f'loss: {loss} - lobj: {loss_item[0]} - lcls: {loss_item[1]} - lbox: {loss_item[2]}')
+        desc = '%10.4s' * 5 % (epoch, lobj.item(), lcls.item(), lbox.item(), total_loss.item())
+        progress.set_description(desc)
         
         # end batch
     scheduler.step()
     # end epoch
+
+    # create checkpoint
+    checkpoint = { 
+        'epoch': epoch,
+        'model': model.state_dict(),
+        'optimizer': optimizer.state_dict(),
+        'scheduler': scheduler.state_dict()
+    }
+    torch.save(checkpoint, create_exp_folder(epoch, model_name) + os.sep + 'checkpoint.pth')
+    
     
     
---
+++ utils/helper.py
@@ -0,0 +1,13 @@
+import os
+
+def create_exp_folder(exp, model_name):
+    exp_root_folder = 'runs'
+    exp_folder_name = f'{exp}_{model_name}'
+    path = exp_root_folder + os.sep + exp_folder_name
+    
+    if not os.path.isdir(exp_root_folder): # create if root exp folder not exist
+        os.mkdir(exp_root_folder)
+    if not os.path.isdir(path): # create if exp folder does not exist
+        os.mkdir(path)
+        
+    return path
